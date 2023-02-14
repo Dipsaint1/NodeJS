81f8513596a73f76c79c360774356f2ae182d087
Cyber9ja <ayodejioladapo96@gmail.com>
2023-02-14 10:12:26 +0000

    added first package

--- https.js
+++ https.js
@@ -1,4 +1,7 @@
-const { send, read } = require('./internals/index.js');
+// const { send, read } = require('./internals/index.js');
+
+const { send } = require('./request');
+const { read } = require('./response'); 
 
 function makeRequest(url, data) {
   send(url, data);
